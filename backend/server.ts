16701dd8837f8de50c8ebde8caa02b1990c248d9
Shridhar Kamat <shridharkamat10@gmail.com>
2025-07-29 09:23:23 +0530

    Revert "feat: first one"
    This reverts commit 780fa63498e7c207ce9fc01ac3cf3253c43bd7a7.

--- backend/server.ts
+++
@@ -1,8 +0,0 @@
-//@ts-ignore
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://zcpesqgzeeiewddphdwg.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjcGVzcWd6ZWVpZXdkZHBoZHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NTE3OTksImV4cCI6MjA2OTEyNzc5OX0.xz3X8mi9M6KZGkS0Nkj-YLPCcTHAx4zwdYXVoPA_8IU"
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
